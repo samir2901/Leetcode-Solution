9a9e412c18f552a0c5b011cc4ea6aa00167c71d8
Samiruddin Thunder <thunder.cse2018@gmail.com>
2020-02-08 22:13:15 +0530

    Solution to problem-1342 added

---
+++ prob_1342/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace prob_1342
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Steps: " + NumberOfSteps(x));
+        }
+
+        static int NumberOfSteps (int num) {
+        int steps=0;
+        while(num!=0){
+            if(num % 2==0){
+                num = num/2;
+                steps += 1;
+            }else{
+                num = num-1;
+                steps += 1;
+            }
+        }
+        return steps;
+    }
+    }
+}

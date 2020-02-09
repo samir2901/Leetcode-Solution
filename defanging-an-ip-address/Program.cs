feb6a9424290c5d9618937fbc05fbe8662055988
Samiruddin Thunder <thunder.cse2018@gmail.com>
2020-02-09 12:50:08 +0530

    Update

---
+++ defanging-an-ip-address/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace defanging_an_ip_address
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter ipv4: ");
+            string ip = Console.ReadLine();
+            Console.WriteLine(DefangIPaddr(ip));            
+        }
+        static string DefangIPaddr(string address) {
+            string x = string.Join("[.]",address.Split('.'));
+            return x;
+        }
+    }
+}

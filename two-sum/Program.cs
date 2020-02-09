0b46717172c3cba15a41625cceff20b564dfa3ac
Samiruddin Thunder <thunder.cse2018@gmail.com>
2020-02-09 12:23:40 +0530

    Update

---
+++ two-sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace prob_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {2,7,11,15};
+            Console.Write("Enter a number: ");
+            int target = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("[{0}]",string.Join(", ", TwoSum(arr,target)));
+        }
+        static int[] TwoSum(int[] nums, int target){
+            int i=0;
+            int j=0;
+            for(i=0;i<nums.Length;i++){
+                for(j=i+1;j<nums.Length;j++){
+                    if(nums[i] + nums[j] == target){
+                        int[] arr = new int[] {i,j};
+                        return arr;
+                    }
+                }
+            }
+            return new int[] {-1,-1};                        
+        }
+    }
+}

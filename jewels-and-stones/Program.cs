b817f606376b162706488e7ef9ac7d489b4c3924
Samiruddin Thunder <thunder.cse2018@gmail.com>
2020-02-20 08:54:37 +0530

    Create Jewels and Stones

---
+++ jewels-and-stones/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace jewels_and_stones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string J, S;
+            Console.Write("Enter J: ");
+            J = Console.ReadLine();
+            Console.Write("Enter S: ");
+            S = Console.ReadLine();
+            Console.WriteLine(NumJewelsInStones(J,S));        
+        }
+        static int NumJewelsInStones(string J, string S){
+            var count = 0;
+            foreach (var character in S)            
+            {
+                if(J.Contains(character)){
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}

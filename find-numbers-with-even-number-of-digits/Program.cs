54b01f55f1ecdaaef30aa14c9cb2cc22486444a0
Samiruddin Thunder <thunder.cse2018@gmail.com>
2020-02-20 09:18:58 +0530

    Create Array having Even Numbers

---
+++ find-numbers-with-even-number-of-digits/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace find_numbers_with_even_number_of_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {12,345,2,6,7896};
+            Console.WriteLine(FindNumbers(array));
+            
+        }
+        static int FindNumbers(int[] nums){
+            int count = 0;
+            foreach (var num in nums)
+            {
+                if ((Convert.ToString(num)).Length % 2 == 0){
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}

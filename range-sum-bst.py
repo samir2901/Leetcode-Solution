cfc9e29625fa79c639ea49c6351be74e6448c383
samir2901 <thunder.cse2018@gmail.com>
2021-06-04 09:56:50 +0530

    update

---
+++ range-sum-bst.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int: 
+        s = 0
+        if root == None:
+            return s
+        elif root.val >= low and root.val <= high:
+            s += root.val            
+        s += self.rangeSumBST(root.left,low,high) + self.rangeSumBST(root.right,low,high)
+        return s
+        

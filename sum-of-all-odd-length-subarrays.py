940ec3963ff405ef27c9dc03f122ba12e6361b07
samir2901 <thunder.cse2018@gmail.com>
2021-06-13 20:42:36 +0530

    update

---
+++ sum-of-all-odd-length-subarrays.py
@@ -0,0 +1,36 @@
+'''
+Example 1:
+
+Input: arr = [1,4,2,5,3]
+Output: 58
+Explanation: The odd-length subarrays of arr and their sums are:
+[1] = 1
+[4] = 4
+[2] = 2
+[5] = 5
+[3] = 3
+[1,4,2] = 7
+[4,2,5] = 11
+[2,5,3] = 10
+[1,4,2,5,3] = 15
+If we add all these together we get 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
+Example 2:
+
+Input: arr = [1,2]
+Output: 3
+Explanation: There are only 2 subarrays of odd length, [1] and [2]. Their sum is 3.
+Example 3:
+
+Input: arr = [10,11,12]
+Output: 66
+'''
+class Solution:
+    def sumOddLengthSubarrays(self, arr):
+        s = 0
+        for i in range(0,len(arr)):
+            num_freq = ((i+1)*(len(arr)-i) + 1)//2 
+            s += num_freq*arr[i]
+        return s
+
+sol = Solution()
+print(sol.sumOddLengthSubarrays([10,11,12]))
